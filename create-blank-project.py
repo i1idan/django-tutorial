9798eeba6688d09205ce919633c93bfaf49878a9
Maryam Saeedi <61593022+i1idan@users.noreply.github.com>
2020-06-14 11:51:01 +0000

    Create create-blank-project.py

---
+++ create-blank-project.py
@@ -0,0 +1,22 @@
+## create-blank-project
+
+mkdir src
+cd src
+
+
+django-admin startproject trydjango
+
+ls
+manage.py  trydjango
+
+
+python manage.py runserver
+
+Performing system checks...
+
+June 14, 2020 - 11:42:00
+Django version 2.0.7, using settings 'trydjango.settings'
+Starting development server at http://127.0.0.1:8000/
+Quit the server with CONTROL-C.
+
+copy url in web browser
