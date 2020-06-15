1e1b5775d662db95e605930690f147abd42aba27
Maryam Saeedi <61593022+i1idan@users.noreply.github.com>
2020-06-15 10:30:53 +0000

    Create change-model.py

---
+++ change-model.py
@@ -0,0 +1,16 @@
+
+### changes in models.py
+
+
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+ titel       = models.CharField(max_length=120) 
+ description = models.TextField(blank = True , null = True)
+ price       = models.DecimalField(decimal_places = 2, max_digits = 1000 )
+ summary     = models.TextField()
+ featured    = models.BooleanField(default = True) #null = True , default = True
+
+python manage.py makemigrations
+python manage.py migrate
